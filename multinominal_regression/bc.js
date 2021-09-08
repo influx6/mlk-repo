f43560bc7ab7a99b2ec65c4bcf4bb0dc3aa70b1d
Alex Ewetumo <trinoxf@gmail.com>
2021-09-09 01:02:14 +0800

    UPT: Initial changes for multinominal

--- multinominal_regression/bc.js
+++ multinominal_regression/bc.js
@@ -1,34 +1,8 @@
 require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 
-const readBooks = 1 // => reading books
-const watchMovies = 0 // => watching movies
-
-/* step 1. encode your choices into numeric form (binary form if two: 0 and 1)
-const dataset = [
-	[5, watchMovies],
-	[15, watchMovies],
-	[25, readBooks],
-	[35, readBooks],
-	[45, readBooks],
-]
-
-step 2:
- use a different equation from linear regression where we can ensure the values
- are between our encoded ranges:
-
- eq => 1 / 1 + e of (-(m * b + x))
-
- Called the Sigmoid Equation, where e is a constant 2.718.
-
- Sigmoid equation produces a output between 0 and 1.
-
- Generalized form is: 1 / 1 + e^z, where z is our equation: -(m * b + x)
-*/
-
-
 // LinearRegression with gradient descent using Batches
-class LogisticRegression {
+class MultiNominalLogisticRegression {
 	constructor(features, labels, options) {
 		this.labels = tf.tensor(labels);
 		this.options = Object.assign({
@@ -43,7 +17,7 @@ class LogisticRegression {
 			.concat(this.features, 1);
 
 		// gradient descent vars
-		this.weights = tf.zeros([this.features.shape[1], 1]);
+		this.weights = tf.zeros([this.features.shape[1], this.labels.shape[1]]);
 		this.crossEntropyHistory = [];
 		this.mean = null;
 		this.variance = null;
@@ -147,4 +121,4 @@ class LogisticRegression {
 	}
 }
 
-module.exports = LogisticRegression
+module.exports = MultiNominalLogisticRegression
